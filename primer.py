ba1f0812f5a4cb7d6f7451b03746f3f56e760ee2
JanZobec <105631428+JanZobec@users.noreply.github.com>
2025-05-13 14:13:41 +0200

    Update primer.py

--- primer.py
+++ primer.py
@@ -7,8 +7,10 @@ root.withdraw() # skrije začetno okno
 root.wm_attributes('-topmost', 1) # skrije začetno okno
 root.geometry("1500x1200") # nastavi velikost okna
 
+začetna_datoteka = r'C:\Users'
+
 # Primer excel in/ali PDF
-xlsx = filedialog.askopenfilename(parent=root, initialdir='Documents', title='Izberi Excel datoteko', filetypes=[("Excel datoteke", ".xlsx .xls")]) # show an "Open" dialog box and return the path to the selected file 
-pdf = filedialog.askopenfilename(parent=root, initialdir='Documents', title='Izberi PDF datoteko', filetypes=[("PDF datoteke", ".pdf")]) # show an "Open" dialog box and return the path to the selected file 
+xlsx = filedialog.askopenfilename(parent=root, initialdir=začetna_datoteka, title='Izberi Excel datoteko', filetypes=[("Excel datoteke", ".xlsx .xls")]) # show an "Open" dialog box and return the path to the selected file 
+pdf = filedialog.askopenfilename(parent=root, initialdir=začetna_datoteka, title='Izberi PDF datoteko', filetypes=[("PDF datoteke", ".pdf")]) # show an "Open" dialog box and return the path to the selected file 
 
 root.destroy()
